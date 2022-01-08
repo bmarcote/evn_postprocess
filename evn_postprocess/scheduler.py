bf99d90320a06b560e3b1a9c481e4f873ec66306
Benito <marcote@jive.eu>
2022-01-08 20:39:35 +0100

    Updates in PolConvert reading

--- evn_postprocess/scheduler.py
+++ evn_postprocess/scheduler.py
@@ -92,11 +92,7 @@ def ms_operations(exp: experiment.Experiment):
 
 def tconvert(exp: experiment.Experiment):
     output = dispatcher(exp, (eee.tconvert, eee.polConvert))
-    did_polConvert_run = False
-    for a_pass in exp.correlator_passes:
-        if len(a_pass.antennas.polconvert) > 0:
-            did_polConvert_run = True
-    if did_polConvert_run:
+    if len(exp.antennas.polconvert) > 0:
         #TODO: if polconvert runs, then create again the MS and run standardplots
         pass
     exp.store()
