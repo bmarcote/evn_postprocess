aaa0cc6ee3050df1bc9cb3c6120eb68dccba02a4
Benito <marcote@jive.eu>
2022-01-17 13:51:36 +0100

    Adding warning for missing log files

--- evn_postprocess/process_pipe.py
+++ evn_postprocess/process_pipe.py
@@ -56,8 +56,9 @@ def get_files_from_vlbeer(exp):
                 exp.antennas[ant].antabfsfile = True
 
     exp.log(f"# Log files found for:\n# {', '.join(exp.antennas.logfsfile)}", False)
+    exp.log(f"# Missing files for:\n# {', '.join(set(exp.antennas.names)-set(exp.antennas.logfsfile))}\n", False)
     exp.log(f"# Antab files found for:\n# {', '.join(exp.antennas.filefsfile)}", False)
-    exp.log(f"# Missing ANTAB files for:\n# {', '.join(set(exp.antennas.names)-set(exp.antennas.filefsfile))}", False)
+    exp.log(f"# Missing files for:\n# {', '.join(set(exp.antennas.names)-set(exp.antennas.antabfsfile))}", False)
     return True
 
 
