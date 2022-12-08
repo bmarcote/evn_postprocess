206ca6bc95db26d3aa35ec8fe090cfda44e2162c
bmarcote <marcote@jive.eu>
2022-12-08 13:09:58 +0100

    Testing on new input options

--- app_test.py
+++ app_test.py
@@ -14,6 +14,9 @@ from evn_postprocess.evn_postprocess import experiment
 from evn_postprocess.evn_postprocess import scheduler as sch
 from evn_postprocess.evn_postprocess import dialog
 from evn_postprocess.evn_postprocess import environment as env
+from evn_postprocess.evn_postprocess import process_ccs as ccs
+from evn_postprocess.evn_postprocess import process_eee as eee
+from evn_postprocess.evn_postprocess import process_pipe as pipe
 
 
 __version__ = '1.0a'
@@ -33,15 +36,63 @@ all_steps = {'setting_up': sch.setting_up_environment,
              'pipeline': sch.pipeline,   # TODO:  sch.protect_archive_data
              'postpipe': sch.after_pipeline,
              'last': sch.final_steps}
+# all_exec_command = {'dirs': Command(env.create_all_dirs, "Creates the required folders (in eee and jop83): " \
+#                                        "eee:/data0/{supsci}/{EXP}, jop83:$IN/{exp}, $OUT/{exp}", "$IN/{supsci}/{exp}"),
+#                     'copyfiles': Command(env.copy_files, "Copies the .vix, .expsum, .piletter, .key/sum  to eee."),
+#                     '': Command(eee.set_credentials, "Sets/recovers the credentials for this experiment."),
+#                     '': Command(pipe.get_files_from_vlbeer, ""),
+#                     'createlis': Command(ccs.create_lis_files, "Create the .lis files in ccs."),
+#                     'getlis': Command(ccs.get_lis_files, "Copies the .lis files from ccs to eee."),
+#                     'modlis': Command(eee._get_passes_from_lisfiles, "Reads the correlator passes from the lis files" \
+#                                                                      " and updates the header."),
+#                     'checklis': Command(env.check_lisfiles, "Runs checklis.py in all .lis files."),
+#                     'getdata': Command(eee.getdata, "Runs getdata.pl."),
+#                     'j2ms2': Command(eee.j2ms2, "Runs j2ms2 with the specified params (modify them with the 'edit'" \
+#                                                 "command)."),
+#                     'expname': Command(eee.update_ms_expname, "Runs expname.py (for e-EVN experiments)."),
+#                     'metadata': Command(eee.get_metadata_from_ms, "Retrieves the observational metadata from the MS."),
+#                     'standardplots': Command(eee.standardplots, "Runs standardplots."),
+#                     'gv': Command(eee.open_standardplot_files, "Opens the standardplots files with gv."),
+#                     'ysfocus': Command(eee.ysfocus, "Runs ysfocus.py"),
+#                     'polswap': Command(eee.polswap, "Runs polswap.py"),
+#                     'flag_weights': Command(eee.flag_weights, "Runs flag_weights.py"),
+#                     'onebit': Command(eee.onebit, "Runs onebit.py"),
+#                     'piletter': Command(eee.update_piletter, "Updates the PI letter with info on MS and PolConvert."),
+#                     'tconvert': Command(eee.tconvert, "Runs tConvert"),
+#                     'polconvert': Command(eee.polconvert, "Runs PolConvert (or prepares files to run it manually)."),
+#                     'postpolconvert': Command(eee.post_polconvert, "Runs all required steps after run PolConvert."),
+#                     'archive': Command(eee.archive, "Runs archive in eee"),
+#                     '': Command(pipe.run_antab_editor, ""),
+#                     '': Command(pipe.create_uvflg, ""),
+#                     '': Command(pipe.create_input_file, ""),
+#                     '': Command(pipe.run_pipeline, ""),
+#                     '': Command(pipe.comment_tasav_files, ""),
+#                     '': Command(pipe.pipeline_feedback, ""),
+#                     '': Command(pipe.archive, ""),
+#                     '': Command(eee.append_antab, ""),
+#                     '': Command(pipe.ampcal, ""),
+#                     '': Command(eee.create_pipelet, ""),
+#                     '': Command(eee.send_letter, ""),
+#                     '': Command(eee.antenna_feedback, ""),
+#                     '': Command(eee.nme_report, "")}
+# supsciers = ('agudo', 'bayandina', 'blanchard', 'burns', 'immer', 'marcote', 'minnie', 'murthy', 'nair', 'oh',
+#              'orosz', 'paragi', 'rmc', 'surcis', 'yang')
 
 exp = None
 app = typer.Typer(help="Runs the full post-processing of an EVN experiment.", rich_markup_mode='rich')
+
+class Command(object):
+    def __init__(self, command, doc: str):
+        """Executes the command (which must be a Python function), that has the associated doc string for help.
+        """
+        self.command = command
+        self.doc = doc
 
 
 @app.command()
 def run(step1: Optional[str] = typer.Argument(None, help='Start the post-processing from this step.'),
         step2: Optional[str] = typer.Argument(None, help='Run the post-processing until this step (included).'),
-        j2ms2par: str = typer.Option(None, help='Additional attributes for j2ms2 (like the fo:).' \
+        j2ms2par: str = typer.Option(None, help='Additional attributes for j2ms2 (like the fo:XXXXXX).' \
                                                                 ' Comma separated if multiple.')):
     """[bold]Runs the post-process from a given step[/bold].
 
@@ -91,7 +142,8 @@ def run(step1: Optional[str] = typer.Argument(None, help='Start the post-process
         elif (exp.last_step is not None) and (step1 is None):
             the_steps = step_keys[step_keys.index(exp.last_step)+1:]
             exp.log(f"Starting after the last sucessful step from a previous run ({exp.last_step}).", False)
-            print(f"[italic]Starting after the last sucessful step from a previous run ({exp.last_step})[/italic].")
+            rich.print("[italic]Starting after the last sucessful step from a previous run " \
+                       f"({exp.last_step})[/italic].")
         # step1 is not None
         elif step2 is None:
             assert step1 in step_keys, f"The introduced step1 {step1} is not recognized from the list {step_keys}."
@@ -124,10 +176,9 @@ def run(step1: Optional[str] = typer.Argument(None, help='Start the post-process
         print('\n\nStopped for manual interaction (see above). Re-run once you have done your duty.')
         return
 
-    print('\nThe post-processing has finished properly.')
-
-
-
+    exp.last_step = "Finished."
+    exp.store()
+    rich.print('\n[italic green]The post-processing has finished properly.[/italic green]')
 
 
 @app.command()
@@ -184,16 +235,17 @@ def edit(param: str, value: str):
                     exp_src.type = experiment.SourceType.fringefinder
 
     exp.store()
-    print('Changes properly stored for experiment.')
+    rich.print('[italic green]Changes properly stored for experiment.[/italic green]')
     raise typer.Exit()
 
 
 @app.command()
 def info():
-    """[bold]Shows the info related to the given experiment (all what postprocess knows until this moment).[/bold]
+    """[bold]Shows the info related to the given experiment (all what postprocess knows until the presentmoment).[/bold]
 
     It will also write this information down into a 'notes.md' file is this does not exist.
     """
+    print(exp)
     exp.print_blessed(outputfile='notes.md')
     raise typer.Exit()
 
@@ -202,7 +254,8 @@ def info():
 def last():
     """Returns the last step that run successfully from post-process in this experiment.
     """
-    print("\n\n" + f"The last step that run for this experiment was {exp.last_step}.")
+    rich.print("\n\n" + f"[italic]The last step that successfully run for this experiment was " \
+               f"[green]{exp.last_step}[/green][/italic].")
     raise typer.Exit()
 
 
@@ -221,28 +274,60 @@ def version_callback(value: bool):
 # typer.Exit(code=0)
 # typer.launch("https://typer.tiangolo.com")
 
-@app.callback()
+@app.callback(help="""Runs the full post-processing steps on an EVN Experiment.
+
+    It will go through the steps between correlation to distribution in a semi-automatic way.
+    Both the experiment name and associated support scientist would be retrieved assuming that this is
+    executed from a folder following [italic]/data0/{supsci}/{EXPNAME}[/italic] path.
+    Otherwise, they can be manually specifed with the --expname and --supsci options.
+
+    There are some commands that can be used with postprocess:
+        - [green]run[/green] : the basic mode. To run the actuall post-process.
+            With no extra arguments: it will run everything from the beginning (or last previous step that run).
+            With STEP1 argument: it runs from STEP1 until the end.
+            With STEP1 STEP2 arguments: it runs from STEP1 until STEP2 (both included).
+        - [green]info[/green] : shows the info related to the experiment (all what is known until this moment).
+        - [green]edit[/green] : edits some of the parameters associated to the experiment. --help for more info.
+        - [green]last[/green] : shows the last successful step that run for this experiment.
+""")
 def main(version: Optional[bool] = typer.Option(None, "--version", callback=version_callback, is_eager=True),
          expname: str = typer.Option(None, help="Experiment code (case insensitive)."),
          supsci: str = typer.Option(None, help='Associated JIVE Support Scientist (surname; case insensitive).')):
     """Runs the full post-processing steps on an EVN Experiment.
 
-    By default it will retrieve the experiment name and associated Support Scientist from the current
-    directory where this script runs. Otherwise they can be manually specified.
+    It will go through the steps between correlation to distribution in a semi-automatic way.
+    Both the experiment name and associated support scientist would be retrieved assuming that this is
+    executed from a folder following [italic]/data0/{supsci}/{EXPNAME}[/italic] path.
+    Otherwise, they can be manually specifed with the --expname and --supsci options.
+
+    There are some commands that can be used with postprocess:
+        - [green]run[/green] : the basic mode. To run the actuall post-process.
+            With no extra arguments: it will run everything from the beginning (or last previous step that run).
+            With STEP1 argument: it runs from STEP1 until the end.
+            With STEP1 STEP2 arguments: it runs from STEP1 until STEP2 (both included).
+        - [green]info[/green] : shows the info related to the experiment (all what is known until this moment).
+        - [green]edit[/green] : edits some of the parameters associated to the experiment. --help for more info.
+        - [green]last[/green] : shows the last successful step that run for this experiment.
     """
     if expname is None:
         expname = Path.cwd().name
-        print(f"\nAssuming the experiment code is {expname}.")
+
+    assert env.grep_remote_file('jops@ccs', '/ccs/var/log2vex/MASTER_PROJECTS.LIS', expname.upper()) != '', \
+        f"The experiment name {expname} is not recognized (not present in MASTER_PROJECTS). " \
+        "You may need to manually specify with --expname"
 
     if supsci is None:
         supsci = Path.cwd().parent.name
-        print(f"Assuming the JIVE Support Scientist is {supsci}.\n")
 
+    assert supsci in supsciers, f"It seems like the JIVE Support Scientist {supsci} is not recognized " \
+                                f"from my database. You may need to use the --supsci option or ask for support."
+
+    global exp
     exp = experiment.Experiment(expname, supsci)
     exp.gui = dialog.Terminal()
 
     if exp.exists_local_copy():
-        print('Restoring stored information from a previous run.')
+        print('A local copy from a previous run has been found and restored.')
         exp = exp.load()
 
 
